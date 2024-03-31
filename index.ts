a34b6d25763b0e352acb43bdca3b5d40d8dc3abc
syedumarali <57311114+syedumarali@users.noreply.github.com>
2024-03-31 15:37:52 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+
+let answers=await inquirer.prompt([
+    {message:"Enter First Number",type:"number", name:"firstNumber"},
+    {message:"Enter Second Number",type:"number", name:"secondNumber"},
+    {
+        message:"Select one Operator to perform Operations",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subtraction","Multiplication","Division"]
+    },
+]);
+
+//conditional statements
+if(answers.operator==="Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator==="Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+
+}
+else if(answers.operator==="Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+
+}
+else if(answers.operator==="Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
